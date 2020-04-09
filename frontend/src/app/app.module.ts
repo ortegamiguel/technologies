be20267c156c8bb065d4b2e71b373b259ac1ce85
Miguel Ortega <omiguel1992@gmail.com>
2020-04-09 12:44:44 -0400

    Added folder structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TechnologyCardComponent } from './components/technology-card/technology-card.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SearchComponent } from './pages/search/search.component';
+import { TechnologiesComponent } from './pages/technologies/technologies.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TechnologyCardComponent,
+    AboutComponent,
+    SearchComponent,
+    TechnologiesComponent,
+    TechnologyComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
